c2a2cdd50b0fdc48d98ef27f45891069ae313f29
irina-khrolenko <khrolenko.ira@gmail.com>
2024-10-22 11:56:14 +0300

    fix: next configurations

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,11 @@
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = { transpilePackages: ["three"] };
+const nextConfig = {
+  //   transpilePackages: ["three"],
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
